a83018f6055bb8008b66a101ece0af643102d72e
dayoungin-ucsd <dain@ucsd.edu>
2026-05-15 01:57:48 -0700

    Step1
    My first map

--- map.js
+++ map.js
@@ -1,3 +1,14 @@
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoicHN5LWR1Y2siLCJhIjoiY21wNm9iY28yMGhrZjJ0b214a3c1aG13YSJ9.6F9rditT2-WQdOzCDoN5Yg';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
